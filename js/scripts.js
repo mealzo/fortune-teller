e8b8a113a926c75c8d42f24b0964aafe5b9eadcd
amelia ralston-okabayashi and richard laws <amelia@>
2018-10-17 19:56:33 -0700

    add logic to count good and bad luck answers in the survey and display an appropriate fortune.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,48 @@
 $(document).ready(function(){
   $("form#survey").submit(function(event){
     event.preventDefault();
+    $("#results").empty();
     $("#results").show();
+    $("#yourFortune").show();
+
+    var userResponses = [];
     $("input:checkbox[name=survey]:checked").each(function(){
-
-      var surveyResults = $(this).val();
-      console.log(surveyResults);
-      $("#results").append(surveyResults + "<br>");
+      var surveyResults = parseInt($(this).val());
+      userResponses.push(surveyResults);
     });
+    console.log(userResponses);
+    var goodCounter = 0;
+    var badCounter = 0;
+    for (var i = 0; i < userResponses.length; i++) {
+      if (userResponses[i] === 1) {
+        badCounter++;
+      }else if (userResponses[i] === 2) {
+        goodCounter++;
+      }
 
+    }
+    var finalCounter = goodCounter - badCounter;
+    if (finalCounter === -3) {
+      $("#results").text("You are cursed to die alone and unsatisfied, a shame unto your family =(")
+    }
+    else if (finalCounter === -2) {
+      $("#results").text("You will be struck by a car or bus or maybe lightning, but you will probably live.")
+    }
+    else if (finalCounter === -1) {
+      $("#results").text("you will develop diabetes or Lycanthropy.")
+    }
+    else if (finalCounter === 0) {
+      $("#results").text("Everything seems ok I guess.")
+    }
+    else if (finalCounter === 1) {
+      $("#results").text("You will find money or delicious food on the ground")
+    }
+    else if (finalCounter === 2) {
+      $("#results").text("You are cursed... with a blessed day")
+    }
+    else if (finalCounter === 3) {
+      $("#results").text("You will literally live forever and actually enjoy it almost all the time.")
+    }
+    console.log(finalCounter);
   });
 });
-
-// $(document).ready(function(){
-//   $("form#transportation_survey").submit(function(event){
-//     event.preventDefault();
-//     $("#work-responses").show();
-//     $("input:checkbox[name=work-transportation]:checked").each(function(){
-//       var workTransportationMode = $(this).val();
-//       $('#work-responses').append(workTransportationMode + "<br>");
-//     });
-//     $('#transportation_survey').hide();
-//   });
-// });
